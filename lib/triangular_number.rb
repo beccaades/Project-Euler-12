540631358d9a3439601fda82a495675e4c3ed70f
beccaades <rebecca.ades@gmail.com>
2015-09-08 10:41:15 -0400

    added test

--- lib/triangular_number.rb
+++ lib/triangular_number.rb
@@ -1,15 +1,15 @@
 def triangular_number(num)
   counter = 1
-  factors_of_numth_natural_triangle_number = []
+  factors_of_triangle_number = []
 
-  until factors_of_numth_natural_triangle_number.size > num 
-    factors_of_numth_natural_triangle_number = get_factors((1..counter).reduce(:+))
+  until factors_of_triangle_number.size > num 
+    factors_of_triangle_number = get_factors((1..counter).reduce(:+))
     counter += 1
   end
   
-  factors_of_numth_natural_triangle_number.max
+  factors_of_triangle_number.max
 end
 
-def get_factors(numth_natural_triangle_number)
-  1.upto(numth_natural_triangle_number).select { |factor| numth_natural_triangle_number % factor == 0}
+def get_factors(current_triangle_num)
+  1.upto(current_triangle_num).select { |factor| current_triangle_num % factor == 0}
 end 
--- spec/triangular_number_spec.rb
+++ spec/triangular_number_spec.rb
@@ -2,5 +2,10 @@ describe "#triangular_number" do
   it "returns the correct number when the number of divisors is higher than five" do
     expect(triangular_number(5)).to eq(28)
   end
-
 end
+
+describe "#get_factors" do
+  it "returns the factors of a number in an array" do
+    expect(get_factors(15)).to eq([1,3,5,15])
+  end
+end 
